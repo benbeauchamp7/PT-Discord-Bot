8f9b3ee833fa84cc6aa579cebcf94fad5cf1706c
Ben Beauchamp <benbeauchamp7@gmail.com>
2020-08-31 16:04:08 -0500

    Extended !survey message persistance to one hour

--- commands/survey.js
+++ commands/survey.js
@@ -3,9 +3,9 @@ module.exports = {
     name: 'survey',
     description: 'Links a google survey for feedback',
     async execute(message) {
-        message.channel.send('https://docs.google.com/forms/d/e/1FAIpQLScBD2YuvAWCFR9Jf6gkS2z9bMgk9xDTkVfh_he_Nw8noJ92oQ/viewform \nThis message will disappear in 5 minutes').then(reply => {
-			reply.delete({'timeout': 300000});
-			message.delete({'timeout': 300000});
+        message.channel.send('https://docs.google.com/forms/d/e/1FAIpQLScBD2YuvAWCFR9Jf6gkS2z9bMgk9xDTkVfh_he_Nw8noJ92oQ/viewform \nThis message will disappear in 1 hour').then(reply => {
+			reply.delete({'timeout': 3600000});
+			message.delete({'timeout': 3600000});
 		})
     }
 }
