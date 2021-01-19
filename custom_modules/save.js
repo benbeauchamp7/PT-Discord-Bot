08631376d17ec4869371046c026212a3b475a961
Ben Beauchamp <benbeauchamp7@gmail.com>
2021-01-19 14:11:35 -0600

    Fixing proc

--- custom_modules/save.js
+++ custom_modules/save.js
@@ -2,6 +2,16 @@ const readline = require('readline');
 const fs = require('fs');
 const config = JSON.parse(fs.readFileSync("config.json", 'utf8'));
 
+const AWS = require('aws-sdk');
+const s3ID = process.env.AWS_ACCESS_KEY_ID;
+const s3KEY = process.env.AWS_SECRET_ACCESS_KEY;
+const s3Bname = process.env.S3_BUCKET_NAME;
+
+const s3 = new AWS.S3({
+    accessKeyId: s3ID,
+    secretAccessKey: s3KEY
+});
+
 module.exports = {
     // queue maps courses to {user, time} objects
 	saveQueue: function(queue) {
@@ -29,6 +39,21 @@ module.exports = {
                 });
             }
         }
+
+        // Load file back into RAM
+        const content = fs.readFileSync(config['queue-file-path']);
+        const params = {
+            Bucket: s3Bname,
+            Key: config['queue-file-path'],
+            Body: content
+        };
+
+        s3.upload(params, function(err, data) {
+            if (err) {
+                console.log(">> The file could not saved to S3 <<");
+                console.log(err)
+            };
+        });
     },
 
     loadQueue: function() {
@@ -38,6 +63,21 @@ module.exports = {
         for (course of config['course-channels']) {
             queue.set(course, []);
         }
+
+        // Snag file from S3
+        const params = {
+            Bucket: s3Bname,
+            Key: config['queue-file-path'],
+        };
+
+        s3.getObject(params, function(err, data) {
+            if (err) {
+                console.log(">> The file could not loaded from S3 <<");
+                console.log(err)
+            };
+
+            fs.writeFileSync(config['queue-file-path'], config['queue-file-path']);
+        })
 
 
         // Make sure the file exists
@@ -58,6 +98,7 @@ module.exports = {
                 queue.get(data[0]).push( {user: data[1], time: parseInt(data[2], 10), ready: data[3] === 'true' || data[3] === undefined ? true : false} );
             });
         }
+
         return queue;
     }
 }
