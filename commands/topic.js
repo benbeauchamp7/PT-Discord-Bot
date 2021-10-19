8895b696561c3a03ffba531ca474aa78150e68db
Ben Beauchamp <benbeauchamp7@gmail.com>
2021-10-18 22:55:49 -0500

    Added /topic

--- commands/topic.js
+++ commands/topic.js
@@ -2,10 +2,47 @@ const fs = require('fs');
 const logger = require('../custom_modules/logging.js');
 const config = JSON.parse(fs.readFileSync("config.json", 'utf8'));
 const CommandError = require('../custom_modules/commandError.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     name: 'topic',
     description: 'Renames a student chat room set',
+    slashes: [
+        new SlashCommandBuilder()
+            .setName('topic')
+            .setDescription('Renames a temporary chat room')
+            .addStringOption(option => 
+                option.setName('name')
+                    .setDescription('The new name of the chat room')
+                    .setRequired(true))
+    ],
+
+    permissions: {
+        topic: {
+            permissions: [{
+                id: '804540323367354388',
+                type: 'ROLE',
+                permission: true
+            }]
+        }
+    },
+
+    async executeInteraction(interaction, data) {
+        const chan = interaction.channel;
+        const newName = interaction.options.getString('name');
+
+        if (!chan.parent.name.endsWith(config['student-chan-specifier']) && !chan.parent.name.endsWith(config['sticky-chan-specifier'])) {
+            await interaction.reply({content: `You can only use this command in student-created discussion rooms`, ephemeral: true});
+            throw new CommandError("/topic wrong room", `${interaction.member}`);
+        }
+
+        const suffix = (chan.parent.name.endsWith(config['student-chan-specifier'])) ? config['student-chan-specifier'] : config['sticky-chan-specifier'];
+        chan.parent.setName(`${newName} ${suffix}`);
+        chan.setName(newName).then(() => {
+            interaction.reply({content: "Done!", ephemeral: false});
+        });
+    },
+
     async execute(message, args) {
 
         const timeout = config['bot-alert-timeout'];
