b206d0511798b459cebc8297c4bfc6e21e05a01d
Ben Beauchamp <benbeauchamp7@gmail.com>
2021-10-20 23:21:06 -0500

    Added /online and /offline

--- commands/offline.js
+++ commands/offline.js
@@ -2,11 +2,42 @@ const logger = require('../custom_modules/logging.js');
 const fs = require('fs');
 const config = JSON.parse(fs.readFileSync("config.json", 'utf8'));
 const CommandError = require('../custom_modules/commandError.js');
-const save = require('../custom_modules/save.js')
+const save = require('../custom_modules/save.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     name: 'offline',
     description: 'Adds the "Off the Clock" role',
+    slashes: [
+        new SlashCommandBuilder()
+            .setName('offline')
+            .setDescription('Marks you as Off the Clock')
+    ],
+
+    permissions: {
+        offline: {
+            permissions: [{
+                id: '731672600367071273',
+                type: 'ROLE',
+                permission: true
+            }]
+        }
+    },
+
+    async executeInteraction(interaction, data) {
+        if (interaction.member.roles.cache.find(r => ["Off the Clock", "Peer Teacher"].includes(r.name))) {
+            interaction.member.roles.add("743870484898250753"); // Add off the clock
+            interaction.member.roles.remove("731672600367071273"); // Remove PT
+
+            let goodbyes = ['¡Adiós!', 'Arrivederci!', 'Au Revoir!', 'Adeus!', 'Auf Wiedersehen!', 'Sayōnara!', 'Do svidaniya!', 'Annyeong!', 'Tot ziens!'];
+            interaction.reply({content: goodbyes[Math.floor(Math.random() * goodbyes.length)], ephemeral: true});
+            logger.log("/online", `${interaction.member}`);
+            return true;
+        }
+
+        throw new CommandError("/online incorrect person", `${interaction.member}`);
+    },
+
     async execute(message, args, options) {
         let queues = options.queues;
         if (message.member.roles.cache.find(r => ["Off the Clock", "Peer Teacher", "Professor"].includes(r.name))) {
--- commands/online.js
+++ commands/online.js
@@ -2,12 +2,41 @@ const logger = require('../custom_modules/logging.js');
 const fs = require('fs');
 const config = JSON.parse(fs.readFileSync("config.json", 'utf8'));
 const CommandError = require('../custom_modules/commandError.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     name: 'online',
     description: 'Removes the "Off the Clock" role',
+    slashes: [
+        new SlashCommandBuilder()
+            .setName('online')
+            .setDescription('Marks you as an online peer teacher')
+    ],
+
+    permissions: {
+        online: {
+            permissions: [{
+                id: '743870484898250753',
+                type: 'ROLE',
+                permission: true
+            }]
+        }
+    },
+
+    async executeInteraction(interaction, data) {
+        if (interaction.member.roles.cache.find(r => ["Off the Clock", "Peer Teacher"].includes(r.name))) {
+            interaction.member.roles.remove("743870484898250753"); // Remove off the clock
+            interaction.member.roles.add("731672600367071273"); // Restore PT
+            interaction.reply({content: "Welcome back!", ephemeral: true});
+            logger.log("/online", `${interaction.member}`);
+            return true;
+        }
+
+        throw new CommandError("/online incorrect person", `${interaction.member}`);
+    },
+
     async execute(message) {
-        if (message.member.roles.cache.find(r => ["Off the Clock", "Peer Teacher", "Professor"].includes(r.name))) {
+        if (message.member.roles.cache.find(r => ["Off the Clock", "Peer Teacher"].includes(r.name))) {
             message.guild.members.fetch(message.author).then((user) => { user.roles.remove("743870484898250753"); }); // Remove off the clock
             message.guild.members.fetch(message.author).then((user) => { user.roles.add("731672600367071273"); }); // Restore PT
             
